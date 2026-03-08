dc2e8aa34eec823018228a28c8a88937de6cb06d
SoundwaveAI <ghost10.town10000@gmail.com>
2026-03-08 21:39:31 +0300

    main of course

--- kuchukbaeva.alyona/S1/main.cpp
+++ kuchukbaeva.alyona/S1/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
 int main()
-{}
+{
+  kuchukbaeva::OutList seque;
+  try {
+    kuchukbaeva::prInput(std::cin, seque);
+    return kuchukbaeva::execLogic(seque, std::cout, std::cerr);
+  } catch (const std::exception& e) {
+    std::cerr << "Fatal except: " << e.what() << "\n";
+    return 2;
+  } catch (...) {
+    std::cerr << "Fatal error" << "\n";
+    return 2;
+  }
+}
 
