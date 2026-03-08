e98ddd8a3786f17a004d3807ee020b13ebdcb222
SoundwaveAI <ghost10.town10000@gmail.com>
2026-03-08 23:12:50 +0300

    fix

--- kuchukbaeva.alyona/S1/test_main.cpp
+++ kuchukbaeva.alyona/S1/test_main.cpp
@@ -1,6 +1,14 @@
-#define BOOST_TEST_MODULE S1
 #include <boost/test/included/unit_test.hpp>
 #include "list.hpp"
+
+#if defined(__GNUC__) || defined(__clang__)
+__attribute__((weak)) int main(int argc, char* argv[])
+{
+  return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
+}
+#endif
+
+#define BOOST_TEST_MODULE S1
 
 BOOST_AUTO_TEST_SUITE(ListTests)
 
